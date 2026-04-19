c37559fc3715b75225dbe6bf31e989fd431828a8
Elizabeth Stein <purplegumdropz@gmail.com>
2026-04-18 20:22:33 -0400

    fix: match webpack output filename to script tag in index.html

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'index.pack.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
